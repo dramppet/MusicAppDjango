ddf495a1bb65f288807a9180cb78418d11623659
DragomirPetko <dramppet@abv.bg>
2025-06-20 16:56:44 +0300

    add details and delete profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from typing import Optional
 
-# Create your views here.
+from django.db.models import QuerySet
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, DeleteView
+
+from common.utils import get_profile
+from profiles.models import Profile
+
+
+class ProfileDetailsView(DetailView):
+    model = Profile
+    template_name = 'profile-details.html'
+
+    def get_object(self, queryset: Optional[QuerySet]=None) -> QuerySet:
+        return get_profile()
+
+class ProfileDeleteView(DeleteView):
+    model = Profile
+    template_name = 'profile-delete.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset: Optional[QuerySet]=None) -> QuerySet:
+        return get_profile()
